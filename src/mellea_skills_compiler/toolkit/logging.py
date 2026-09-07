5c653b300eda82ed5a4ecc0edc95d4487817bf62
Dhaval Salwala <dhavalsalwala@gmail.com>
2026-09-07 09:42:07 +0100

    Fix environment variable name for log level

--- src/mellea_skills_compiler/toolkit/logging.py
+++ src/mellea_skills_compiler/toolkit/logging.py
@@ -8,7 +8,7 @@ logging.getLogger("ai_atlas_nexus").setLevel(logging.WARNING)
 logging.getLogger("faiss.loader").setLevel(logging.ERROR)
 logging.getLogger("cpex.framework.manager").setLevel(logging.ERROR)
 logging.getLogger("mellea").setLevel(logging.ERROR)
-os.environ["MELLEA_LOG_LEVEL"] = "ERROR"
+os.environ["MELLEA_LOGS_LEVEL"] = "ERROR"
 
 # ANSI color codes
 COLORS = {
